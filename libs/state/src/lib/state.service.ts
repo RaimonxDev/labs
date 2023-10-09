668db31d17d56360c39a0f78abd226a863226171
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2023-10-09 18:56:21 -0300

    mejoras en el tipado

--- libs/state/src/lib/state.service.ts
+++ libs/state/src/lib/state.service.ts
@@ -1,104 +1,89 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 
-export interface StoreState {
-    key: string;
-    state: any;
-}
-
-export type Store = Map<string, any>;
-
-
+export type Store<T> = Map<keyof T, BehaviorSubject<T[keyof T]>>;
 @Injectable({ providedIn: 'root' })
-export class StateService {
+export class StateService<T> {
 
-  store: Store = new Map<string, any>();
-  _store$ = new BehaviorSubject<any>(null);
+  private store: Store<T> = new Map<keyof T, BehaviorSubject<T[keyof T]>>();
+  private _store$ = new BehaviorSubject<any>(null);
+  private initialState: Partial<T> = {};
 
   constructor() { }
 
-
-  initialState(store: Store) {
-    this.store = store;
-  }
-
-  createState<T>(store: T[]) {
-    store.forEach((item: any) => {
-      Object.keys(item).forEach((key) => {
-        if (this.checkStateIfExist(key)) {
-          console.error(`State ${key} already exist`);
-          return;
-        }
-        this.store.set(key, new BehaviorSubject(item[key]))
-      })
-    })
-    this.updateStore();
-  }
-
-  getState<T>(key: keyof T): Observable<any> {
-    return this.store.get(key as string);
-  }
-
-  setState<T>(key: keyof T, value: any) {
-    if (!this.checkStateIfExist(key as string)) {
-      console.error(`State ${key as string} not exist`);
-      return;
-    }
-    this.store.get(key as string).next({
-      ...this.getValuetFromStore(key as string), // get current value
-      ...value
+  createState(initialState: Partial<T>) {
+    this.initialState = { ...initialState };
+    Object.keys(initialState).forEach((key) => {
+      const stateKey = key as keyof T;
+      if (this.checkStateIfExist(stateKey)) {
+        console.error(`State ${stateKey as string} already exist`);
+        return;
+      }
+      this.store.set(stateKey, new BehaviorSubject(initialState[stateKey] as T[keyof T]));
     });
-    this.updateStore();
+    this.emitStore();
   }
 
-  checkStateIfExist(key: string) {
-    return this.store.has(key);
-  }
-
-  resetState<T>(key: keyof T) {
-    if (!this.checkStateIfExist(key as string)) {
+  onState<K extends keyof T>(key: K): Observable<T[K] | null> {
+    if (!this.checkStateIfExist(key)) {
       console.error(`State ${key as string} not exist`);
-      return;
+      return of(null);
     }
-    const allKeys: any[] = Object.keys(this.getValuetFromStore(key as string));
-
-    this.store.get(key as string).next({
-      ...allKeys.reduce((acc, key) => {
-        acc[key] = null;
-        return acc;
-      }, {})
-    })
-    this.updateStore();
+    return this.store.get(key)?.asObservable() as Observable<T[K]>
   }
 
-  resetAllState() {
-    this.store.forEach((value) => {
-      value.next(null);
-    })
-    this.updateStore();
-  }
-
-  getAllStateSnapshopt() {
-    const _storeObject: any = {}
-    this.store.forEach((value, key) => {
-      _storeObject[key] = value.getValue();
-    })
-    return _storeObject;
-  }
-
-  // Return all state as object with key and getValue() from behaviorSubject
-  getAllState$(): Observable<unknown> {
+  onAllState$(): Observable<Partial<T>> {
     return this._store$.asObservable();
   }
 
-  private updateStore = () => {
-    this._store$.next(this.getAllStateSnapshopt());
+  setState(key: keyof T, value: T[keyof T]) {
+    if (!this.checkStateIfExist(key)) {
+      console.error(`State ${key as string} not exist`);
+      return;
+    }
+    this.store.get(key)?.next({
+      ...this.store.get(key)?.getValue(),
+      ...value
+    });
+    this.emitStore();
   }
-  private getValuetFromStore(key: string) {
-    return this.store.get(key).getValue();
+
+  resetState(key: keyof T) {
+    if (!this.checkStateIfExist(key)) {
+      console.error(`State ${key as string} not exist`);
+      return;
+    }
+    const initialValue = this.initialState[key];
+    this.store.get(key)?.next(initialValue as T[keyof T]);
+    this.emitStore();
   }
 
+  resetAllState() {
+    this.store.forEach((_, key) => {
+      const initialValue = this.initialState[key];
+      this.store.get(key)?.next(initialValue as T[keyof T]);
+    });
+    this.emitStore();
+  }
 
+  getSnapshot(key: keyof T): any {
+    return this.store.get(key)?.getValue();
+  }
 
+  getAllStateSnapshot(): Partial<T> {
+    const _storeObject: Partial<T> = {};
+    this.store.forEach((value, key) => {
+      _storeObject[key] = value.getValue();
+    });
+    return _storeObject;
+  }
+
+  private checkStateIfExist(key: keyof T) {
+    return this.store.has(key);
+  }
+
+  private emitStore = () => {
+    this._store$.next(this.getAllStateSnapshot());
+  }
 
 }
--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
-import { StateService } from '../../../../../libs/state/src';
-import { user } from 'app/mock-api/common/user/data';
 import { MatButtonModule } from '@angular/material/button';
+import { StateService } from '../../../../../libs/state/src';
 
 export interface StoreExample {
-  step1: {
-    data: any;
-    otherField: any;
-  },
-  step2: {
-    data: any;
-    user: any;
-    filters: {
-      name: string;
-      age: number;
-    }
+  step1: Step1,
+  step2: Step2,
+  user?: User
+}
+
+export interface Step1 {
+  data: any;
+  otherField: any;
+}
+export interface Step2 {
+  data: any;
+  user: any;
+  filters: {
+    name: string;
+    age: number;
   }
 }
 
-const storeExample: StoreExample[] = [{
+const dataExample: StoreExample = {
   step1: {
     data: [
       {
@@ -35,8 +38,17 @@ const storeExample: StoreExample[] = [{
       name: 'ramon',
       age: 25
     }
+  },
+  user: {
+    name: 'ramon',
+    age: 25
   }
-}]
+}
+
+export interface User {
+  name: string;
+  age: number;
+}
 @Component({
   selector: 'example',
   standalone: true,
@@ -49,35 +61,38 @@ const storeExample: StoreExample[] = [{
 })
 export class ExampleComponent implements OnInit {
 
-  stateService = inject(StateService);
+  stateService: StateService<StoreExample> = inject(StateService);
+
   constructor() { }
 
   ngOnInit(): void {
-    this.stateService.createState(storeExample);
+    this.stateService.createState(dataExample);
 
-    this.stateService.getState<StoreExample>('step2').subscribe((data) => {
-      console.log(data);
+    this.stateService.onState('user').subscribe((data) => {
+      console.log('user', data);
     })
 
-    this.stateService.getAllState$().subscribe((data: StoreExample) => {
-      console.log('getAllState$', data);
-    })
+    this.stateService.getSnapshot('step1')
 
+    // this.stateService.getState('step2').subscribe((data) => {
+    //   console.log(data);
+    // })
 
+    // this.stateService.getAllState$().subscribe((data: StoreExample) => {
+    //   console.log('getAllState$', data);
+    // })
   }
 
+
   changeState() {
-    this.stateService.setState<StoreExample>('step2', {
-      data: [1, 2, 3, 4, 5],
-      user: {
-        id: 1,
-        name: 'ramon'
-      },
+    this.stateService.setState('user', {
+      name: 'pepe',
+      age: 25
     })
   }
 
   resetState() {
-    this.stateService.resetState<StoreExample>('step2');
+    // this.stateService.resetState('step2');
   }
   resetAllState() {
     this.stateService.resetAllState();
